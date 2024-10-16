130d8ac87c39ed40b58789803302227e9e206f1b
Trong Quy <trongquy1628@gmail.com>
2024-10-17 05:59:53 +0700

    Extract Annotations

--- src/main/java/com/springjpa/demoJPA/exception/ErrorCode.java
+++ src/main/java/com/springjpa/demoJPA/exception/ErrorCode.java
@@ -8,13 +8,13 @@ import org.springframework.http.HttpStatusCode;
 public enum ErrorCode {
     USER_EXISTED(1001, "User already existed", HttpStatus.INTERNAL_SERVER_ERROR),
     UNCATEGORIZED_EXCEPTION(999, "Uncategorized exception", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003, "Username is invalid",HttpStatus.BAD_REQUEST),
-    INVALID_PASSWORD(1004, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "Username must be at least {min} characters",HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1004, "Password must be at least {min} characters", HttpStatus.BAD_REQUEST),
     INVALID_KEY(1005, "Invalid message key", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1006, "User not existed", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1007, "UNAUTHENTICATED", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1008, "You do not have permission", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1009, "Invalid date of birth", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1009, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
 
     ;
     private int code;
--- src/main/java/com/springjpa/demoJPA/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springjpa/demoJPA/exception/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.springjpa.demoJPA.exception;
 
 import com.springjpa.demoJPA.dto.request.ApiResponse;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @ControllerAdvice
     public class GlobalExceptionHandler {
+        private static final String MIN_ATTRIBUTE = "min";
+
         @ExceptionHandler(value = Exception.class)
         ResponseEntity<ApiResponse> runtimeExceptionHandler(RuntimeException e) {
             ApiResponse apiResponse = new ApiResponse();
@@ -21,15 +28,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
         ResponseEntity<ApiResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
             ApiResponse apiResponse = new ApiResponse();
             String enumKey = e.getFieldError().getDefaultMessage();
+
             ErrorCode errorCode = ErrorCode.INVALID_KEY;
+
+            Map<String, Object> attributes = null;
             try {
                 errorCode = ErrorCode.valueOf(enumKey);
+
+                var constrainViolation = e.getBindingResult()
+                        .getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+                attributes = constrainViolation.getConstraintDescriptor().getAttributes();
+
             }catch (IllegalArgumentException ex) {
 
             }
 
             apiResponse.setCode(errorCode.getCode());
-            apiResponse.setMessage(errorCode.getMessage());
+            apiResponse.setMessage(Objects.nonNull(attributes) ?
+                    mapAttribute(errorCode.getMessage(), attributes)
+                    : errorCode.getMessage());
+
             return ResponseEntity.badRequest().body(apiResponse);
         }
 
@@ -51,5 +70,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
                             .message(errorCode.getMessage())
                             .build()
             );
+        }
+
+        private String mapAttribute(String message, Map<String, Object> attributes) {
+            String minValue = attributes.get(MIN_ATTRIBUTE).toString();
+            return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
         }
 }
